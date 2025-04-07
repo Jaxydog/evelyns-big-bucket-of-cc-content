f0adee6eb559d8303e01113c6e70563eafeb2b87
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-04-07 05:49:46 -0400

    fix: prevent usage of non-default paths, use proper modname for require, and ensure that packages can be required at all

--- lua/libraries/external-require.lua
+++ lua/libraries/external-require.lua
@@ -9,7 +9,6 @@ local module = {
 
 ---@class evelyn.RequireOptions
 ---
----@field public dir? string The file path to store the downloaded file.
 ---@field public url? string The URL at which to download the file.
 ---@field public forceGet? boolean Whether to re-download the file even if it's already stored locally.
 
@@ -22,8 +21,12 @@ local module = {
 ---
 ---@return T module The required module.
 function externalRequire(name, options)
-    local baseDir = options and options.dir or module.defaultDir
+    local baseDir = module.defaultDir
     local file = ('%s/%s.lua'):format(baseDir, name)
+
+    if not package.path:match('/%.evelyns%-libraries/%?%.lua') then
+        package.path = package.path .. ';/.evelyns-libraries/?.lua'
+    end
 
     if (options and options.forceGet) or not fs.exists(file) then
         local baseUrl = options and options.url or module.defaultUrl
@@ -36,7 +39,7 @@ function externalRequire(name, options)
         assert(shell.run('wget', url, file), 'Failed to download dependency')
     end
 
-    return require(file)
+    return require(name)
 end
 
 return setmetatable(module, {
