64d4bdac7c902194846642797c690995a7c590e5
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-04-07 05:31:27 -0400

    feat: add external require call library

---
+++ lua/libraries/external-require.lua
@@ -0,0 +1,44 @@
+---@class evelyn.RequireLib
+local module = {
+    ---The base URL used to download dependencies.
+    defaultUrl =
+    'https://raw.githubusercontent.com/Jaxydog/evelyns-big-bucket-of-cc-content/refs/heads/main/lua/libraries',
+    ---The directory in which to download files.
+    defaultDir = '/.evelyns-libraries',
+}
+
+---@class evelyn.RequireOptions
+---
+---@field public dir? string The file path to store the downloaded file.
+---@field public url? string The URL at which to download the file.
+---@field public forceGet? boolean Whether to re-download the file even if it's already stored locally.
+
+---Requires a file that is hosted externally.
+---
+---@generic T The type of the imported module.
+---
+---@param name string The name of the library.
+---@param options? evelyn.RequireOptions Additional options for this require call.
+---
+---@return T module The required module.
+function externalRequire(name, options)
+    local baseDir = options and options.dir or module.defaultDir
+    local file = ('%s/%s.lua'):format(baseDir, name)
+
+    if (options and options.forceGet) or not fs.exists(file) then
+        local baseUrl = options and options.url or module.defaultUrl
+        local url = ('%s/%s.lua'):format(baseUrl, name)
+
+        fs.makeDir(baseDir)
+
+        if fs.exists(file) then fs.delete(file) end
+
+        assert(shell.run('wget', url, file), 'Failed to download dependency')
+    end
+
+    return require(file)
+end
+
+return setmetatable(module, {
+    __call = function(_, ...) return externalRequire(...) end
+})
