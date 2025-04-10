674eb6841d7f87fd9a726ac5d3a4349465d7c9a6
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-04-10 15:55:05 -0400

    feat: split console into two smaller libraries, add additional functionality to both

---
+++ lua/libraries/logging.lua
@@ -0,0 +1,80 @@
+---A log level.
+---@alias evelyn.logging.level
+---| 'debug' Debug logging.
+---| 'info' Informational logging.
+---| 'warn' Warning logging.
+---| 'error' Error logging.
+
+---A color choice.
+---@alias evelyn.logging.colorChoice
+---| 'auto' Automatically enable color if it's available.
+---| 'always' Always enable color, even if it's not available.
+---| 'never' Never enable color, even if it's available.
+
+---A logging function.
+---
+---@alias evelyn.logging.function fun(module: evelyn.logging.lib, format: string, ...: unknown): number
+
+---Provides better logging functionality than the standard `print` and `printError` functions.
+---
+---@class evelyn.logging.lib
+local module = {}
+
+---Determines when logs should be output with color.
+---
+---@type evelyn.logging.colorChoice
+module.color = 'auto'
+
+---Determines the level of logs that should be output.
+---
+---@type evelyn.logging.level | nil
+module.level = 'info'
+
+---Returns `true` if logging functions will output using color.
+---
+---@return boolean color Whether to use color.
+function module:shouldUseColor()
+    return self.color == 'always' or (self.color == 'auto' and term.isColor())
+end
+
+---Creates a new log function with the given level, color, and predicate.
+---
+---@param level evelyn.logging.level The log level.
+---@param color ccTweaked.colors.color The color to use if colors are enabled.
+---@param predicate fun(module: evelyn.logging.lib): boolean A function that determines whether the log should be output.
+---
+---@return evelyn.logging.function function The logging function.
+function module.createLogFunction(level, color, predicate)
+    return function(moduleTable, format, ...)
+        if not predicate(moduleTable) then return 0 end
+
+        local shouldUseColor = moduleTable:shouldUseColor()
+
+        if shouldUseColor then term.setTextColor(color) end
+
+        local lines = write(('[%s] '):format(level))
+
+        if shouldUseColor then term.setTextColor(colors.white) end
+
+        return lines + print(format:format(...))
+    end
+end
+
+---Outputs a debug log.
+module.logDebug = module.createLogFunction('debug', colors.magenta, function(moduleTable)
+    return moduleTable.level == 'debug'
+end)
+---Outputs an informational log.
+module.logInfo = module.createLogFunction('info', colors.lightBlue, function(moduleTable)
+    return moduleTable.level == 'debug' or moduleTable.level == 'info'
+end)
+---Outputs a warning log.
+module.logWarn = module.createLogFunction('warn', colors.yellow, function(moduleTable)
+    return moduleTable.level == 'debug' or moduleTable.level == 'info' or moduleTable.level == 'warn'
+end)
+---Outputs a warning log.
+module.logError = module.createLogFunction('error', colors.red, function(moduleTable)
+    return moduleTable.level ~= nil
+end)
+
+return module
---
+++ lua/libraries/query.lua
@@ -0,0 +1,221 @@
+---A set of additional options to be passed into read functions.
+---
+---@class evelyn.query.options
+---
+---@field public maxAttempts? integer The maximum number of attempts to read a value before returning `nil`.
+---@field public replaceCharacter? string The character used to replace all input text.
+---@field public history? string[] The input history.
+---@field public completeFunction? (fun(partial: string): string[]) A function that handles auto-completion.
+---@field public default? string The default input string, if validation fails.
+
+---Provides better terminal input than the standard `read` function.
+---
+---@class evelyn.query.lib
+local module = {}
+
+---The default prompt builder for read functions.
+---
+---@param prompt string The prompt string.
+---
+---@return string prompt The prompt.
+function module.defaultPromptBuilder(prompt, _)
+    return ('%s: '):format(prompt)
+end
+
+---Creates a new read function with the given prompt construction and type validation functions.
+---
+---@generic T The type returned by the created function.
+---
+---@param promptBuilder fun(prompt: string, default?: string): string A function that creates the prompt based on the given string.
+---@param validator fun(input: string): boolean, T | nil, string | nil A function that validates a given input string.
+---
+---@return fun(prompt: string, options?: evelyn.query.options): T | nil function The read function.
+function module.createReadFunction(promptBuilder, validator)
+    return function(promptInput, options)
+        ---@type evelyn.query.options
+        options = options or {}
+
+        if options.default and not validator(options.default) then
+            error('Default value is invalid')
+        end
+
+        local attempts = 0
+        local prompt = promptBuilder(promptInput, options.default)
+        local _, startY = term.getCursorPos()
+
+        while attempts <= (options.maxAttempts or math.huge) do
+            term.setCursorPos(1, startY)
+            term.clearLine()
+
+            write(prompt)
+
+            term.setTextColor(colors.gray)
+
+            local input = read(options.replaceCharacter, options.history, options.completeFunction, options.default)
+            local valid, value, reason = validator(input:match('^%s*(.-)%s*$'))
+
+            if valid then return value end
+
+            printError(('Invalid value%s'):format(reason and (': %s'):format(reason) or ''))
+            sleep(1)
+
+            local _, currentY = term.getCursorPos()
+
+            term.setCursorPos(1, currentY - 1)
+            term.clearLine()
+
+            attempts = attempts + 1
+        end
+
+        return nil
+    end
+end
+
+---Reads a string from the terminal.
+---
+---The return value will only ever be `nil` if the `maxAttempts` field is set.
+module.readString = module.createReadFunction(module.defaultPromptBuilder, function(input)
+    return true, input, nil
+end)
+
+---Reads a string from the terminal that matches the given pattern.
+---
+---The return value will only ever be `nil` if the `maxAttempts` field is set.
+---
+---@param prompt string The prompt string.
+---@param pattern string The pattern to match.
+---@param options? evelyn.query.options The read options.
+---
+---@return string | nil input The input value.
+function module.readStringMatching(prompt, pattern, options)
+    return module.createReadFunction(module.defaultPromptBuilder, function(input)
+        if input:match(pattern) then
+            return true, input, nil
+        else
+            return false, nil, ('should match %s'):format(pattern)
+        end
+    end)(prompt, options)
+end
+
+---Reads a boolean from the terminal.
+---
+---The return value will only ever be `nil` if the `maxAttempts` field is set.
+module.readBoolean = module.createReadFunction(function(prompt, default)
+    if default and default:match('^[nN]|no|false$') then
+        return ('%s [y/N]: '):format(prompt)
+    else
+        return ('%s [Y/n]: '):format(prompt)
+    end
+end, function(input)
+    if input:match('^[yY]|yes|true$') then
+        return true, true, nil
+    elseif input:match('^[nN]|no|false$') then
+        return true, false, nil
+    end
+
+    return false, nil, 'expected boolean'
+end)
+
+---Reads a number from the terminal.
+---
+---The return value will only ever be `nil` if the `maxAttempts` field is set.
+module.readNumber = module.createReadFunction(module.defaultPromptBuilder, function(input)
+    local numeric = tonumber(input)
+
+    if numeric == nil then
+        return false, nil, 'expected number'
+    end
+
+    return true, numeric, nil
+end)
+
+---Reads a number from the terminal that is within the given range.
+---
+---The return value will only ever be `nil` if the `maxAttempts` field is set.
+---
+---@param prompt string The prompt string.
+---@param min? number The minimum allowed value.
+---@param max? number The maximum allowed value.
+---@param options? evelyn.query.options The read options.
+---
+---@return number | nil input The input value.
+function module.readNumberInRange(prompt, min, max, options)
+    if min == nil and max == nil then
+        return module.readNumber(prompt, options)
+    end
+
+    return module.createReadFunction(module.defaultPromptBuilder, function(input)
+        local numeric = tonumber(input)
+
+        if numeric == nil then
+            return false, nil, 'expected number'
+        end
+
+        if min ~= nil and numeric < min then
+            return false, nil, ('expected >= %s'):format(min)
+        elseif max ~= nil and numeric > max then
+            return false, nil, ('expected <= %s'):format(max)
+        end
+
+        return true, numeric, nil
+    end)(prompt, options)
+end
+
+---Reads an integer from the terminal.
+---
+---The return value will only ever be `nil` if the `maxAttempts` field is set.
+module.readInteger = module.createReadFunction(module.defaultPromptBuilder, function(input)
+    local numeric = tonumber(input)
+
+    if numeric == nil then
+        return false, nil, 'expected number'
+    end
+
+    local integer, decimal = math.modf(numeric)
+
+    if decimal ~= 0 then
+        return false, nil, 'expected integer'
+    end
+
+    return true, integer, nil
+end)
+
+---Reads an integer from the terminal that is within the given range.
+---
+---The return value will only ever be `nil` if the `maxAttempts` field is set.
+---
+---@param prompt string The prompt string.
+---@param min? integer The minimum allowed value.
+---@param max? integer The maximum allowed value.
+---@param options? evelyn.query.options The read options.
+---
+---@return integer | nil input The input value.
+function module.readIntegerInRange(prompt, min, max, options)
+    if min == nil and max == nil then
+        return module.readInteger(prompt, options)
+    end
+
+    return module.createReadFunction(module.defaultPromptBuilder, function(input)
+        local numeric = tonumber(input)
+
+        if numeric == nil then
+            return false, nil, 'expected number'
+        end
+
+        local integer, decimal = math.modf(numeric)
+
+        if decimal ~= 0 then
+            return false, nil, 'expected integer'
+        end
+
+        if min ~= nil and integer < min then
+            return false, nil, ('expected >= %s'):format(min)
+        elseif max ~= nil and integer > max then
+            return false, nil, ('expected <= %s'):format(max)
+        end
+
+        return true, integer, nil
+    end)(prompt, options)
+end
+
+return module
