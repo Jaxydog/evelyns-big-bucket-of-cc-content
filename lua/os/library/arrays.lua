439d926929f8f50e44df0645474e7db4e831a2e5
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-04-20 16:30:16 -0400

    feat(os): add array and table utility libraries

---
+++ lua/os/library/arrays.lua
@@ -0,0 +1,207 @@
+---Provides convenience methods for arrays.
+---
+---@class evelyn.os.library.arrays.Lib
+local module = {}
+
+---Creates a new array containing `length` values returned by the callback.
+---
+---@generic T The element type.
+---
+---@param length integer The length.
+---@param callback fun(index: integer): T The callback.
+---
+---@return T[] array The new array.
+function module.new(length, callback)
+    local array = {}
+
+    for index = 1, length, 1 do
+        array[index] = callback(index)
+    end
+
+    return array
+end
+
+---Returns a new array containing values that have been mapped by the given callback.
+---
+---@generic T The element type.
+---@generic U The mapped type.
+---
+---@param array T[] The array.
+---@param callback fun(index: integer, value: T): U The callback.
+---
+---@return U[] array The new array.
+function module.map(array, callback)
+    local mapped = {}
+
+    for index, value in ipairs(array) do
+        mapped[#mapped + 1] = callback(index, value)
+    end
+
+    return mapped
+end
+
+---Returns a new array containing all values with their duplicates removed.
+---
+---@generic T The element type.
+---
+---@param array T[] The array.
+---@param predicate? fun(lhs: T, rhs: T): equal: boolean The predicate.
+---
+---@return T[] array The new array.
+function module.dedupe(array, predicate)
+    predicate = predicate or function(lhs, rhs)
+        return lhs == rhs
+    end
+
+    local deduped = {}
+
+    for _, value in ipairs(array) do
+        local index = module.find(deduped, function(_, other)
+            return predicate(other, value)
+        end)
+
+        if index == nil then
+            deduped[#deduped + 1] = value
+        end
+    end
+
+    return deduped
+end
+
+---Returns a value computed by the given callback.
+---
+---@generic T The element type.
+---@generic U The folded type.
+---
+---@param array T[] The array.
+---@param initial U The initial value.
+---@param callback fun(current: U, index: integer, value: T): U The callback.
+---
+---@return U value The folded value.
+function module.fold(array, initial, callback)
+    for index, value in ipairs(array) do
+        initial = callback(initial, index, value)
+    end
+
+    return initial
+end
+
+---Returns the first index for which the predicate returns `true`.
+---
+---@generic T The element type.
+---
+---@param array T[] The array.
+---@param predicate fun(index: integer, value: T): boolean The predicate.
+---
+---@return integer? index The array index.
+function module.find(array, predicate)
+    for index, value in ipairs(array) do
+        if predicate(index, value) then
+            return index
+        end
+    end
+end
+
+---Returns the first mapped value for which the callback returns non-nil.
+---
+---@generic T The element type.
+---@generic U The mapped type.
+---
+---@param array T[] The array.
+---@param callback fun(index: integer, value: T): U | nil The callback.
+---
+---@return U? value The mapped value.
+function module.findMap(array, callback)
+    for index, value in ipairs(array) do
+        local mapped = callback(index, value)
+
+        if mapped ~= nil then
+            return mapped
+        end
+    end
+end
+
+---Returns a new array containing only values for which the predicate returns `true`.
+---
+---@generic T The element type.
+---
+---@param array T[] The array.
+---@param predicate fun(index: integer, value: T): boolean The predicate.
+---
+---@return T[] array The new array.
+function module.filter(array, predicate)
+    local filtered = {}
+
+    for index, value in ipairs(array) do
+        if predicate(index, value) then
+            filtered[#filtered + 1] = value
+        end
+    end
+
+    return filtered
+end
+
+---Returns a new array containing only values for which the callback returns non-nil.
+---
+---@generic T The element type.
+---@generic U The mapped type.
+---
+---@param array T[] The array.
+---@param callback fun(index: integer, value: T): U | nil The callback.
+---
+---@return U[] array The new array.
+function module.filterMap(array, callback)
+    local filterMapped = {}
+
+    for index, value in ipairs(array) do
+        local mapped = callback(index, value)
+
+        if mapped ~= nil then
+            filterMapped[#filterMapped + 1] = mapped
+        end
+    end
+
+    return filterMapped
+end
+
+---Returns a new array containing all values within each inner array.
+---
+---@generic T The element type.
+---
+---@param array T[][] The array.
+---
+---@return T[] array The new array.
+function module.flat(array)
+    local flattened = {}
+
+    for _, inner in ipairs(array) do
+        for _, value in ipairs(inner) do
+            flattened[#flattened + 1] = value
+        end
+    end
+
+    return flattened
+end
+
+---Returns a new array containing all values returned by the callback.
+---
+---@generic T The element type.
+---@generic U The mapped type.
+---
+---@param array T[] The array.
+---@param callback fun(index: integer, value: T): U[] The callback.
+---
+---@return U[] array The new array.
+function module.flatMap(array, callback)
+    local flatMapped = {}
+
+    for index, inner in ipairs(array) do
+        for _, value in ipairs(callback(index, inner)) do
+            flatMapped[#flatMapped + 1] = value
+        end
+    end
+
+    return flatMapped
+end
+
+return module
---
+++ lua/os/library/tables.lua
@@ -0,0 +1,259 @@
+---Provides convenience methods for tables.
+---
+---@class evelyn.os.library.tables.Lib
+local module = {}
+
+---Returns a new array containing the table's keys in an arbitrary order.
+---
+---@generic K The key type.
+---@generic V The value type.
+---
+---@param table table<K, V> The table.
+---
+---@return K[] array The new array.
+function module.keys(table)
+    local array = {}
+
+    for key, _ in pairs(table) do
+        array[#array + 1] = key
+    end
+
+    return array
+end
+
+---Returns a new array containing the table's values in an arbitrary order.
+---
+---@generic K The key type.
+---@generic V The value type.
+---
+---@param table table<K, V> The table.
+---
+---@return V[] array The new array.
+function module.values(table)
+    local array = {}
+
+    for _, value in pairs(table) do
+        array[#array + 1] = value
+    end
+
+    return array
+end
+
+---Returns a new table containing keys and values returned by the callback.
+---
+---@generic K The key type.
+---@generic V The value type.
+---@generic T The mapped key type.
+---@generic U The mapped value type.
+---
+---@param table table<K, V> The table.
+---@param callback fun(key: K, value: V): key: T, value: U The callback.
+---
+---@return table<T, U> table The new table.
+function module.map(table, callback)
+    local mapped = {}
+
+    for key, value in pairs(table) do
+        local mappedKey, mappedValue = callback(key, value)
+
+        mapped[mappedKey] = mappedValue
+    end
+
+    return mapped
+end
+
+---Returns a new table containing all values with their duplicates removed.
+---
+---@generic K The key type.
+---@generic V The value type.
+---
+---@param table table<K, V> The table.
+---@param conflicting? fun(last: K, next: K): K The conflict resolver.
+---@param predicate? fun(lhs: V, rhs: V): equal: boolean The predicate.
+---
+---@return table<K, V> table The new table.
+function module.dedupe(table, conflicting, predicate)
+    conflicting = conflicting or function(last, _)
+        return last
+    end
+    predicate = predicate or function(lhs, rhs)
+        return lhs == rhs
+    end
+
+    local deduped = {}
+
+    for tableKey, tableValue in pairs(table) do
+        local otherKey = module.find(deduped, function(_, otherValue)
+            return predicate(otherValue, tableValue)
+        end)
+
+        if otherKey == nil then
+            deduped[tableKey] = tableValue
+        else
+            deduped[otherKey] = nil
+            deduped[conflicting(otherKey, tableKey)] = tableValue
+        end
+    end
+
+    return deduped
+end
+
+---Returns a value computed by the given callback.
+---
+---@generic K The key type.
+---@generic V The value type.
+---@generic T The folded type.
+---
+---@param table table<K, V> The table.
+---@param initial T The initial value.
+---@param callback fun(current: T, key: K, value: V): T The callback.
+---
+---@return T value The folded value.
+function module.fold(table, initial, callback)
+    for key, value in pairs(table) do
+        initial = callback(initial, key, value)
+    end
+
+    return initial
+end
+
+---Returns the first key and value for which the predicate returns `true`.
+---
+---@generic K The key type.
+---@generic V The value type.
+---
+---@param table table<K, V> The table.
+---@param predicate fun(key: K, value: V): boolean The predicate.
+---
+---@return K? key The key.
+---@return V? value The value.
+function module.find(table, predicate)
+    for key, value in pairs(table) do
+        if predicate(key, value) then
+            return key, value
+        end
+    end
+end
+
+---Returns the first mapped key and value for which the callback returns non-nil.
+---
+---@generic K The key type.
+---@generic V The value type.
+---@generic T The mapped key type.
+---@generic U The mapped value type.
+---
+---@param table table<K, V> The table.
+---@param callback fun(key: K, value: V): key: T | nil, value: U | nil The callback.
+---
+---@return T? key The mapped key.
+---@return U? value The mapped value.
+function module.findMap(table, callback)
+    for key, value in pairs(table) do
+        local mappedKey, mappedValue = callback(key, value)
+
+        if mappedKey ~= nil and mappedValue ~= nil then
+            return mappedKey, mappedValue
+        end
+    end
+end
+
+---Returns a new table containing only keys and values for which the predicate returns `true`.
+---
+---@generic K The key type.
+---@generic V The value type.
+---
+---@param table table<K, V> The table.
+---@param predicate fun(key: K, value: V): boolean The predicate.
+---
+---@return table<K, V> table The new table.
+function module.filter(table, predicate)
+    local filtered = {}
+
+    for key, value in pairs(table) do
+        if predicate(key, value) then
+            filtered[key] = value
+        end
+    end
+
+    return filtered
+end
+
+---Returns a new table containing only keys and values for which the predicate returns non-nil.
+---
+---@generic K The key type.
+---@generic V The value type.
+---@generic T The mapped key type.
+---@generic U The mapped value type.
+---
+---@param table table<K, V> The table.
+---@param callback fun(key: K, value: V): key: T | nil, value: U | nil The callback.
+---
+---@return table<T, U> table The new table.
+function module.filterMap(table, callback)
+    local filterMapped = {}
+
+    for key, value in pairs(table) do
+        local mappedKey, mappedValue = callback(key, value)
+
+        if mappedKey ~= nil and mappedValue ~= nil then
+            filterMapped[mappedKey] = mappedValue
+        end
+    end
+
+    return filterMapped
+end
+
+---Returns a new table containing all keys and values within each inner table.
+---
+---@generic K The key type.
+---@generic V The value type.
+---
+---@param table table<K, table<K, V>> The table.
+---@param conflicting fun(key: K, lastValue: V, nextValue: V): V The conflict resolver.
+---
+---@return table<K, V> table The new table.
+function module.flat(table, conflicting)
+    local flattened = {}
+
+    for _, inner in pairs(table) do
+        for key, value in pairs(inner) do
+            if flattened[key] == nil then
+                flattened[key] = value
+            else
+                flattened[key] = conflicting(key, flattened[key], value)
+            end
+        end
+    end
+
+    return flattened
+end
+
+---Returns a new table containing all keys and values returned by the callback.
+---
+---@generic K The key type.
+---@generic V The value type.
+---@generic T The mapped key type.
+---@generic U The mapped value type.
+---
+---@param table table<K, V> The table.
+---@param callback fun(key: K, value: V): key: T, value: U The callback.
+---@param conflicting fun(key: T, lastValue: U, nextValue: U): U The conflict resolver.
+---
+---@return table<K, V> table The new table.
+function module.flatMap(table, callback, conflicting)
+    local flattened = {}
+
+    for key, value in pairs(table) do
+        local mappedKey, mappedValue = callback(key, value)
+
+        if flattened[mappedKey] == nil then
+            flattened[mappedKey] = mappedValue
+        else
+            flattened[mappedKey] = conflicting(mappedKey, flattened[mappedKey], mappedValue)
+        end
+    end
+
+    return flattened
+end
+
+return module
